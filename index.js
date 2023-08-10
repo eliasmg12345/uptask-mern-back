e9e7d35ed466f4cf2c5637f34087231053413351
eliasmg12345 <randyofgod666@gmail.com>
2023-08-10 16:56:26 -0400

    socket

--- index.js
+++ index.js
@@ -39,6 +39,57 @@ app.use("/api/tareas", tareaRoutes)
 
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, () => {
+const servidor = app.listen(PORT, () => {
     console.log(`servidor corriendo en ${PORT}`);
+})
+
+//SOCKET.IO
+
+import { Server } from 'socket.io'
+
+const io = new Server(servidor, {
+    pingTiemout: 6000,
+    cors: {
+        origin: process.env.FRONTEND_URL,
+    }
+})
+
+
+io.on('connection', (socket) => {
+    console.log('conectado a socket.io');
+
+    //definir los ecventos de socket io
+    /*
+    socket.on('prueba', (nombre) => {
+        console.log('proeba desde socket io', nombre);
+        socket.emit('respuesta', { nombre: "elias" })
+    })
+    */
+
+    socket.on('abrir proyecto', (proyecto) => {
+        socket.join(proyecto)
+        //socket.emit('respuesta', { nombre: "Elias" })
+
+    })
+
+    socket.on('nueva tarea', tarea => {
+        const proyecto = tarea.proyecto
+        socket.to(proyecto).emit('tarea agregada', tarea)
+    })
+
+    socket.on('eliminar tarea', tarea => {
+        const proyecto = tarea.proyecto
+        socket.to(proyecto).emit('tarea eliminada', tarea)
+    })
+
+    socket.on('actualizar tarea', tarea => {
+        const proyecto = tarea.proyecto._id
+        socket.to(proyecto).emit('tarea actualizada', tarea)
+    })
+
+    socket.on('cambiar estado', tarea => {
+        const proyecto = tarea.proyecto._id
+        socket.to(proyecto).emit('nuevo estado', tarea)
+    })
+
 })
